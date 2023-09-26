0b733153cd1e9c6be0a1a22462f22ed0773baf62
JS10123 <jstrayer24@jesuitmail.org>
2023-09-25 17:42:20 -0700

    DriveTrainCommand Starting

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -6,9 +6,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveTrainSubsystem extends SubsystemBase {
-    WPI_TalonSRX frontLeft = new WPI_TalonSRX(Constants.FRONT_LEFT_DRIVE_ID);
-    WPI_TalonSRX frontRight = new WPI_TalonSRX(Constants.FRONT_RIGHT_DRIVE_ID);
-    WPI_TalonSRX backLeft = new WPI_TalonSRX(Constants.BACK_LEFT_DRIVE_ID);
-    WPI_TalonSRX backRight = new WPI_TalonSRX(Constants.BACK_RIGHT_DRIVE_ID);
+    private final WPI_TalonSRX frontLeft = new WPI_TalonSRX(Constants.FRONT_LEFT_DRIVE_ID);
+    private final WPI_TalonSRX frontRight = new WPI_TalonSRX(Constants.FRONT_RIGHT_DRIVE_ID);
+    private final WPI_TalonSRX backLeft = new WPI_TalonSRX(Constants.BACK_LEFT_DRIVE_ID);
+    private final WPI_TalonSRX backRight = new WPI_TalonSRX(Constants.BACK_RIGHT_DRIVE_ID);
+    private static DriveTrainSubsystem instance;
+
+    public DriveTrainSubsystem() {
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
+    public static DriveTrainSubsystem getInstance() {
+        if (instance == null) {
+            instance = new DriveTrainSubsystem();
+        }
+        return instance;
+    }
 
 }
