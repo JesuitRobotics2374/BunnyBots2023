ada063b280af4e79dfe145255a60fbaaf402df4b
AriesPowv <143663708+AriesPowv@users.noreply.github.com>
2023-11-07 14:44:47 -0800

    Update IndexerSubsystem.java

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -10,9 +10,58 @@ public class IndexerSubsystem {
     private WPI_TalonFX indexerOneMotor = new WPI_TalonFX(Constants.INDEXER_MOTOR_ONE);
     private WPI_TalonFX indexerTwoMotor = new WPI_TalonFX(Constants.INDEXER_MOTOR_TWO);
 
-    // private b
+    private boolean m_position1;
+    private boolean m_position2;
+    private boolean m_position3;
 
-    // public IndexerSubsystem() {
+    public IndexerSubsystem() {
+        indexerOneMotor.getSelectedSensorVelocity();
+        indexerTwoMotor.getSelectedSensorVelocity();
 
-    // }
+        m_position1 = false;
+        m_position2 = false;
+        m_position3 = false;
+        
+        //indexerOneMotor.config_kP(0, Constants.SHOOTER_KP);
+        //indexerOneMotor.config_kI(0, Constants.SHOOTER_KI);
+        //indexerOneMotor.config_kD(0, Constants.SHOOTER_KD);
+
+        //indexerTwoMotor.config_kP(0, Constants.SHOOTER_KP);
+        //indexerTwoMotor.config_kI(0, Constants.SHOOTER_KI);
+        //indexerTwoMotor.config_kD(0, Constants.SHOOTER_KD);
+        
+    }
+
+    @Override
+    public void periodic() {
+        updateIndexer()
+    }
+
+    public void forwardToShooter() {
+        
+    }
+
+    public void acceptFromIntake() {
+        
+    }
+
+    public void updateIndexer() {
+        
+    }
+
+    public void cycleIndexerOne() {
+        
+    }
+
+    public void cycleIndexerTwo() {
+        
+    }
+
+
+
+
+
+
+    
+
 }
