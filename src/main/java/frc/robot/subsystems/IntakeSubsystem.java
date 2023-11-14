5826bd0701f22a9a093c4bc602b02847c0419cae
Crusaderbots <jvilla26@jesuitmail.org>
2023-11-13 16:48:25 -0800

    bug fix for index motor connector

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,10 +1,12 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import java.lang.Math;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 
@@ -13,6 +15,6 @@ public class IntakeSubsystem extends SubsystemBase {
     /*
      * 
      */
-    public final WPI_TalonSRX indexer = WPI_TalonSRX(Constants.INTAKE_MOTOR_ONE);
+    public final WPI_TalonSRX indexer = new WPI_TalonSRX(Constants.INTAKE_MOTOR_ONE);
     //public void buttonDown();
 };
